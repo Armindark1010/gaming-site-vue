6097b1d25a99f73e57a94f44a6d4270916309365
armindark1010 <armin10dybala@gmail.com>
2024-06-21 15:43:25 +0330

    ONLINE 2.0

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports =
+//  defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV ===
+//   ''
+// })
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gaming-site-vue/' // به جای 'repository-name'، نام مخزن خودت رو بگذار
+    : '/'
+});
