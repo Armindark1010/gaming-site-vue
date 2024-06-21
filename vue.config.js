2e04c734f1f547f2a5ee9c1c2d40d19437ed865f
armindark1010 <armin10dybala@gmail.com>
2024-06-21 20:37:21 +0330

    first test 1

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/gaming-site-vue/' // به جای 'repository-name'، نام مخزن خودت رو بگذار
+    ? '/test2-of-github-pages/' // به جای 'repository-name'، نام مخزن خودت رو بگذار
     : '/'
 });
